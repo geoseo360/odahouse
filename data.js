df0ecfd1a6e8fbb9c7ec1404c0f76f3bf9535b41
geoseo360 <geoseo360@gmail.com>
2024-05-27 13:56:43 +0400

    commit

---
+++ data.js
@@ -0,0 +1,751 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-oda-house",
+      "name": "Oda House",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -0.3498499983621386,
+        "pitch": 0.13197877613817077,
+        "fov": 1.2967178853431212
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.000159798262970412,
+          "pitch": 0.34063473925023047,
+          "rotation": 0,
+          "target": "1-living-room-1st-floor"
+        },
+        {
+          "yaw": -2.770958575198879,
+          "pitch": 0.1316052664101548,
+          "rotation": 0,
+          "target": "13-territory-near-the-house"
+        },
+        {
+          "yaw": -1.7257349498892651,
+          "pitch": 0.07347266642116068,
+          "rotation": 0,
+          "target": "12-terrace-mountain-view"
+        },
+        {
+          "yaw": -2.225161762098219,
+          "pitch": 0.1658058888698175,
+          "rotation": 0,
+          "target": "10-rest-area"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-living-room-1st-floor",
+      "name": "Living room (1st floor)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -0.023271056693246805,
+        "pitch": 0.0077552143432164655,
+        "fov": 1.4967563682390181
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.018407712748658156,
+          "pitch": 0.020566650982065227,
+          "rotation": 0,
+          "target": "4-bathroom-1st-floor"
+        },
+        {
+          "yaw": -0.6611558204066643,
+          "pitch": 0.07602051058608694,
+          "rotation": 5.497787143782138,
+          "target": "5-2st-floor"
+        },
+        {
+          "yaw": 1.5766180865524593,
+          "pitch": 0.3925029671404747,
+          "rotation": 0,
+          "target": "2-living-room"
+        },
+        {
+          "yaw": 0.6983257584990383,
+          "pitch": 0.08737943415838245,
+          "rotation": 0,
+          "target": "3-wine-room-1st-floor"
+        },
+        {
+          "yaw": 3.0492619361936963,
+          "pitch": 0.06405061147154711,
+          "rotation": 0,
+          "target": "0-oda-house"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-living-room",
+      "name": "Living room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 0.16353035177073494,
+        "pitch": 0.17309076360218967,
+        "fov": 1.4967563682390181
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7254333332493026,
+          "pitch": 0.12485561612940543,
+          "rotation": 0,
+          "target": "0-oda-house"
+        },
+        {
+          "yaw": 1.9585996059844204,
+          "pitch": -0.0430061598428253,
+          "rotation": 0,
+          "target": "5-2st-floor"
+        },
+        {
+          "yaw": 2.2719035224426802,
+          "pitch": -0.028938871719947912,
+          "rotation": 1.5707963267948966,
+          "target": "4-bathroom-1st-floor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-wine-room-1st-floor",
+      "name": "Wine room (1st floor)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -0.2558451901976966,
+        "pitch": 0.16266596644656062,
+        "fov": 1.4967563682390181
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.469579157212883,
+          "pitch": -0.02425225522885377,
+          "rotation": 0,
+          "target": "2-living-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-bathroom-1st-floor",
+      "name": "Bathroom (1st floor)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -0.5794682641396367,
+        "pitch": 0.22311901032420423,
+        "fov": 1.4728486795256832
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5449894330058065,
+          "pitch": 0.1166815372030996,
+          "rotation": 0,
+          "target": "1-living-room-1st-floor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-2st-floor",
+      "name": "2st floor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -3.052652480805815,
+        "pitch": 0.09815001473130991,
+        "fov": 1.3860928260721364
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7878861656566656,
+          "pitch": -0.019079160794705174,
+          "rotation": 0,
+          "target": "6-bedroom-1-2st-floor"
+        },
+        {
+          "yaw": 1.8019758176078051,
+          "pitch": 0.07488691405966641,
+          "rotation": 0,
+          "target": "9-bedroom-3-2st-floor"
+        },
+        {
+          "yaw": -2.5710944768669837,
+          "pitch": 0.068731370814902,
+          "rotation": 0,
+          "target": "7-bedroom-2-2st-floor"
+        },
+        {
+          "yaw": 3.054550331798075,
+          "pitch": 0.10673934252037576,
+          "rotation": 3.141592653589793,
+          "target": "1-living-room-1st-floor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-bedroom-1-2st-floor",
+      "name": "Bedroom 1 (2st floor)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -0.7348311952038955,
+        "pitch": 0.10135444566574492,
+        "fov": 1.4967563682390181
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7556270357628359,
+          "pitch": 0.14147115672094657,
+          "rotation": 0,
+          "target": "5-2st-floor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-bedroom-2-2st-floor",
+      "name": "Bedroom 2 (2st floor)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -0.4539056494989637,
+        "pitch": 0.19388035858017716,
+        "fov": 1.4967563682390181
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.252781238099633,
+          "pitch": 0.14659229465036994,
+          "rotation": 0,
+          "target": "8-terrace-2st-floor"
+        },
+        {
+          "yaw": -0.20568996816293073,
+          "pitch": 0.09496699775004203,
+          "rotation": 0,
+          "target": "5-2st-floor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-terrace-2st-floor",
+      "name": "Terrace (2st floor)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 0.32822413917667603,
+        "pitch": -0.0010592376845117712,
+        "fov": 1.4967563682390181
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7677377963897918,
+          "pitch": 0.09742422569298803,
+          "rotation": 0,
+          "target": "7-bedroom-2-2st-floor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-bedroom-3-2st-floor",
+      "name": "Bedroom 3 (2st floor)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.36078381995576,
+          "pitch": 0.03906816222875875,
+          "rotation": 0,
+          "target": "5-2st-floor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-rest-area",
+      "name": "Rest area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -0.21123463990027247,
+        "pitch": 0.06984184393283499,
+        "fov": 1.4967563682390181
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1112395592889062,
+          "pitch": 0.15583802903962507,
+          "rotation": 0,
+          "target": "11-barbecue-area"
+        },
+        {
+          "yaw": -0.4553570941777956,
+          "pitch": 0.15166845326641365,
+          "rotation": 0,
+          "target": "12-terrace-mountain-view"
+        },
+        {
+          "yaw": 1.286454568397824,
+          "pitch": 0.3036071060583492,
+          "rotation": 0,
+          "target": "0-oda-house"
+        },
+        {
+          "yaw": -2.238169678872632,
+          "pitch": 0.11893421346178101,
+          "rotation": 0,
+          "target": "13-territory-near-the-house"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-barbecue-area",
+      "name": "Barbecue area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 0.010491257087245742,
+        "pitch": 0.28398298600340866,
+        "fov": 1.4937653340108215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0417524160448206,
+          "pitch": 0.2520078398373897,
+          "rotation": 0,
+          "target": "10-rest-area"
+        },
+        {
+          "yaw": 1.8366800805291215,
+          "pitch": 0.20808977751129554,
+          "rotation": 5.497787143782138,
+          "target": "12-terrace-mountain-view"
+        },
+        {
+          "yaw": -1.881491967772833,
+          "pitch": 0.19129751270589246,
+          "rotation": 0,
+          "target": "13-territory-near-the-house"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-terrace-mountain-view",
+      "name": "Terrace mountain view",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 0.11844339377291746,
+        "pitch": 0.13024821525613106,
+        "fov": 1.4967563682390181
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9454516099734729,
+          "pitch": 0.3077402477286917,
+          "rotation": 0,
+          "target": "11-barbecue-area"
+        },
+        {
+          "yaw": -2.506962978273693,
+          "pitch": 0.14311047775767705,
+          "rotation": 0,
+          "target": "13-territory-near-the-house"
+        },
+        {
+          "yaw": 2.949527849229372,
+          "pitch": 0.16600918344015447,
+          "rotation": 4.71238898038469,
+          "target": "0-oda-house"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-territory-near-the-house",
+      "name": "Territory near the house",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 0.10221721535887873,
+        "pitch": 0.20422064437112297,
+        "fov": 1.4967563682390181
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.013094124755639314,
+          "pitch": 0.2719926350376891,
+          "rotation": 0,
+          "target": "0-oda-house"
+        },
+        {
+          "yaw": -0.3155021626835186,
+          "pitch": 0.27573821462915404,
+          "rotation": 4.71238898038469,
+          "target": "10-rest-area"
+        },
+        {
+          "yaw": -1.0287696382855813,
+          "pitch": 0.23807573708458385,
+          "rotation": 0,
+          "target": "11-barbecue-area"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-mountain-view",
+      "name": "Mountain View",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -0.06619532678414153,
+        "pitch": 0.10219623688613666,
+        "fov": 1.4967563682390181
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3539197762765536,
+          "pitch": 0.06405087614861493,
+          "rotation": 3.141592653589793,
+          "target": "0-oda-house"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "15-downside-view",
+      "name": "Downside view",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.4967563682390181
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.00962220144159076,
+          "pitch": -0.1022772143033368,
+          "rotation": 0,
+          "target": "12-terrace-mountain-view"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "ODA HOUSE GEORGIA",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
